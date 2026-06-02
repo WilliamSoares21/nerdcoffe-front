52d6a351c762fb31f7c0f7761b5bfbd6da6471b8
William <pedro.williamsoares2022@gmail.com>
2026-06-02 08:03:28 -0300

    chore: remover next-remove-imports e restaurar compatibilidade com Turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
-import removeImports from 'next-remove-imports'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   allowedDevOrigins: ['172.30.29.80'],
 }
 
-export default removeImports()(nextConfig)
-
+export default nextConfig
